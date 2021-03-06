25bffa451caf3033a1a0e7270826d6ad6d8fe003
Matheus F Martins <martinsmf00@gmail.com>
2021-03-06 18:03:06 -0300

    Allure

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,6 +13,6 @@ Capybara.configure do |config|
 end
 
 AllureCucumber.configure do |config|
-  config.results_directory = "/allure-report"
+  config.results_directory = "/logs"
   config.clean_results_directory = true
 end
